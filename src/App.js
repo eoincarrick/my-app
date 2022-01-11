401a6d1e33c850013447e2df82109f6e5260be68
Eoin Carrick <22carri99@gmail.com>
2022-01-11 17:05:55 +0000

    masses

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import Tes from "./Tes";
-import Mailbox from "./Mailbox";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Tes />
       <Content />
-      <Mailbox />
       <Footer />
     </div>
   );
