28b0444081bfc6b8644ee13d0f9e89bf6af844a2
Eoin Carrick <22carri99@gmail.com>
2022-01-11 02:51:41 +0000

    commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import "./index.css";
+function Footer() {
+  const today = new Date();
+  return (
+    <footer className="footer">
+      <p>Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Groceries List</h1>
+    </header>
+  );
+}
+
+export default Header;
